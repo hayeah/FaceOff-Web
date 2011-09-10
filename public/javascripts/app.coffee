4fb80a3d5437ad971e6868fc0ba06ef58e8176b7
Howard Yeh <hayeah@gmail.com>
2011-09-10 19:34:23 +0800

    convert to coffee script

---
+++ public/javascripts/app.coffee
@@ -0,0 +1,4 @@
+# hi
+$ () ->
+  $("#mark .like").click () ->
+    $("#mark").toggleClass("faceon")
---
+++ public/javascripts/app.js
@@ -0,0 +1,7 @@
+(function() {
+  $(function() {
+    return $("#mark .like").click(function() {
+      return $("#mark").toggleClass("faceon");
+    });
+  });
+}).call(this);
